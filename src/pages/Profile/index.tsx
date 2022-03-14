4955ae0be18afb339cfcf47c16bac4c9f0db4c52
Michel Wene <michelwene@hotmail.com>
2022-03-14 16:27:50 -0300

    finish style RandomCalendar and transiction responsive desktop

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -7,14 +7,32 @@ import {
   RightSide,
   Repos,
   CalendarHeading,
+  Tab,
+  RepoIcon,
 } from "./styles";
 import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
 import RandomCalendar from "../../components/RandomCalendar";
 
 const Profile: React.FC = () => {
+  const TabContent = () => {
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">30</span>
+    </div>;
+  };
+
   return (
     <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset"></span>
+          <TabContent />
+        </div>
+
+        <span className="line"></span>
+      </Tab>
       <Main>
         <LeftSide>
           <ProfileData
@@ -30,6 +48,11 @@ const Profile: React.FC = () => {
           />
         </LeftSide>
         <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
+
           <Repos>
             <h2>Random repos</h2>
             <div>
